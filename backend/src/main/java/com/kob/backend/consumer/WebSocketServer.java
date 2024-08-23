7aa1c5d839d36ca20f3a66d55541b982577850a8
富伟 张 <1378496782@qq.com>
2024-08-23 22:14:05 +0800

    6.2  56:21

--- backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -17,22 +17,26 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 @Component
-@ServerEndpoint("/websocket/{token}")  // WebSocket服务器端点，注意不要以'/'结尾
+@ServerEndpoint("/websocket/{token}")  // WebSocket服务器端点，客户端通过此地址连接
 public class WebSocketServer {
     // 用于存储所有连接的WebSocketServer实例，每个用户对应一个实例
-    private final static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<Integer, WebSocketServer>();
+    public final static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+
+    // 用于存储所有正在匹配的用户
     private final static CopyOnWriteArraySet<User> matchpool = new CopyOnWriteArraySet<>();
 
-    // 当前WebSocket连接的用户
+    // 当前WebSocket连接对应的用户
     private User user;
 
-    // 当前WebSocket的会话
+    // 当前WebSocket的会话对象，用于与客户端通信
     private Session session = null;
 
     // 静态的UserMapper对象，用于从数据库中获取用户信息
     private static UserMapper userMapper;
 
-    // 通过@Autowired注入UserMapper，并赋值给静态变量
+    private Game game = null;
+
+    // 通过@Autowired注解，将UserMapper的实例注入到WebSocketServer类中
     @Autowired
     public void setUserMapper(UserMapper userMapper) {
         WebSocketServer.userMapper = userMapper;
@@ -41,85 +45,116 @@ public class WebSocketServer {
     // 当有新连接建立时调用
     @OnOpen
     public void onOpen(Session session, @PathParam("token") String token) throws IOException {
-        this.session = session;  // 将当前会话赋值给实例变量
-        System.out.println("connected to websocket");  // 输出连接成功的信息
+        this.session = session;  // 将当前会话对象赋值给实例变量
+        System.out.println("connected to websocket");  // 输出连接成功的日志
 
-        // 从路径参数中获取用户ID并转换为Integer
+        // 从路径参数中获取用户ID并转换为Integer类型
         int userId = JwtAuthentication.getUserId(token);
 
-        // 从数据库中查询用户信息并赋值给当前实例的user变量
+        // 从数据库中查询用户信息，并赋值给当前实例的user变量
         user = userMapper.selectById(userId);
 
-        // 将当前用户和WebSocketServer实例添加到用户集合中
+        // 如果用户信息存在，将用户和当前WebSocketServer实例添加到用户集合中
         if (user != null) {
-//            System.out.println("token: " + token);
             users.put(userId, this);
-            System.out.println(user + " connected!");
+            System.out.println(user + " connected!");  // 输出用户连接成功的日志
         } else {
-            session.close();
+            session.close();  // 如果用户信息不存在，关闭当前WebSocket会话
         }
-
-//        System.out.println("Users: " + users);
     }
 
     // 当连接关闭时调用
     @OnClose
     public void onClose() {
-        System.out.println("disconnected from websocket");  // 输出断开连接的信息
+        System.out.println("disconnected from websocket");  // 输出断开连接的日志
 
-        // 如果当前用户不为空，从用户集合中移除该用户
+        // 如果当前用户不为空，从用户集合中移除该用户，并将其从匹配池中移除
         if (user != null) {
             users.remove(user.getId());
             matchpool.remove(user);
         }
     }
 
+    // 开始匹配
     private void startMatching() {
-        System.out.println("start matching");
-        matchpool.add(user);
+        System.out.println("start matching");  // 输出开始匹配的日志
+        matchpool.add(user);  // 将当前用户加入匹配池
 
+        // 当匹配池中有超过一个用户时，进行匹配
         while (matchpool.size() > 1) {
             Iterator<User> it = matchpool.iterator();
-            User a = it.next(), b = it.next();
+            User a = it.next(), b = it.next();  // 取出两个用户进行匹配
             matchpool.remove(a);
             matchpool.remove(b);
 
-            Game game = new Game(13, 14, 20);
-            game.createMap();
+            System.out.println("-----------------");
+            System.out.println("匹配成功！");
+            System.out.println("玩家A：" + a.getUsername());
+            System.out.println("玩家B：" + b.getUsername());
+            System.out.println("-----------------");
 
+            // 创建新的游戏实例
+            Game game = new Game(13, 14, 20, a.getId(), b.getId());
+            game.createMap();  // 创建游戏地图
+
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+
+            game.start();
+
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id", game.getPlayerA().getId());
+            respGame.put("a_sx", game.getPlayerA().getSx());
+            respGame.put("a_sy", game.getPlayerA().getSy());
+            respGame.put("b_id", game.getPlayerB().getId());
+            respGame.put("b_sx", game.getPlayerB().getSx());
+            respGame.put("b_sy", game.getPlayerB().getSy());
+            respGame.put("map", game.getG());
+
+            // 给用户a发送匹配成功的消息，包括对手信息和游戏地图
             JSONObject respA = new JSONObject();
             respA.put("event", "start-matching");
             respA.put("opponent_username", b.getUsername());
             respA.put("opponent_photo", b.getPhoto());
-            respA.put("gamemap", game.getG());
+            respA.put("game", respGame);
             users.get(a.getId()).sendMessage(respA.toJSONString());
 
+            // 给用户b发送匹配成功的消息，包括对手信息和游戏地图
             JSONObject respB = new JSONObject();
             respB.put("event", "start-matching");
             respB.put("opponent_username", a.getUsername());
             respB.put("opponent_photo", a.getPhoto());
-            respB.put("gamemap", game.getG());
+            respB.put("game", respGame);
             users.get(b.getId()).sendMessage(respB.toJSONString());
-
-
         }
     }
 
+    // 停止匹配
     private void stopMatching() {
-        System.out.println("stop matching");
-        matchpool.remove(user);
+        System.out.println("stop matching");  // 输出停止匹配的日志
+        matchpool.remove(user);  // 将当前用户从匹配池中移除
+    }
+
+    private void move(int direction) {
+        if (game.getPlayerA().getId().equals(user.getId())) {
+            game.setNextStepA(direction);
+        } else if (game.getPlayerB().getId().equals(user.getId())) {
+            game.setNextStepB(direction);
+        }
     }
 
     // 当从客户端收到消息时调用
     @OnMessage
     public void onMessage(String message, Session session) {
         System.out.println("received message: " + message);  // 输出接收到的消息
-        JSONObject data = JSONObject.parseObject(message);
-        String event = data.getString("event");
+        JSONObject data = JSONObject.parseObject(message);  // 将接收到的消息解析为JSON对象
+        String event = data.getString("event");  // 获取事件类型
         if ("start-matching".equals(event)) {
-            startMatching();
-        } else {
-            stopMatching();
+            startMatching();  // 如果事件为“start-matching”，开始匹配
+        } else if ("stop-matching".equals(event)) {
+            stopMatching();   // 否则，停止匹配
+        } else if ("move".equals(event)) {
+            move(data.getInteger("direction"));
         }
     }
 
@@ -131,7 +166,7 @@ public class WebSocketServer {
 
     // 发送消息给客户端
     public void sendMessage(String message) {
-        // 同步块，确保在会话的多个线程之间发送消息时的线程安全
+        // 使用同步块，确保在多个线程之间发送消息时的线程安全
         synchronized (session) {
             session.getAsyncRemote().sendText(message);  // 异步发送消息给客户端
         }
--- backend/src/main/java/com/kob/backend/consumer/utils/Game.java
+++ backend/src/main/java/com/kob/backend/consumer/utils/Game.java
@@ -1,19 +1,39 @@
 package com.kob.backend.consumer.utils;
 
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.consumer.WebSocketServer;
+
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Game {
-    final private Integer rows;
-    final private Integer cols;
-    final private Integer inner_walls_count;
-    final private int[][] g;
-    final private int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+public class Game extends Thread {
+    private final Integer rows;
+    private final Integer cols;
+    private final Integer inner_walls_count;
+    private final int[][] g;
+    private final int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+    private final Player playerA, playerB;
+    private Integer nextStepA = 0, nextStepB = 0;
+    private ReentrantLock lock = new ReentrantLock();
+    private String status = "playing";      // playing -> finished
+    private String loser = "";
 
-    public Game(Integer rows, Integer cols, Integer inner_walls_count) {
+    public Player getPlayerA() {
+        return playerA;
+    }
+
+    public Player getPlayerB() {
+        return playerB;
+    }
+
+    public Game(Integer rows, Integer cols, Integer inner_walls_count, int idA, int idB) {
         this.rows = rows;
         this.cols = cols;
         this.inner_walls_count = inner_walls_count;
         this.g = new int[rows][cols];
+        playerA = new Player(idA, rows - 2, 1, new ArrayList<>());
+        playerB = new Player(idB, 1, cols - 2, new ArrayList<>());
     }
 
     public int[][] getG() {
@@ -75,6 +95,113 @@ public class Game {
         for (int i = 0; i < 1000; i ++ ) {
             if (draw())
                 break;
+        }
+    }
+
+    public void setNextStepA(int nextStepA) {
+        lock.lock();
+        try {
+            this.nextStepA = nextStepA;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void setNextStepB(int nextStepB) {
+        lock.lock();
+        try {
+            this.nextStepB = nextStepB;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void judge() {
+
+    }
+
+    private void sendResult() {
+        JSONObject resp = new JSONObject();
+        resp.put("event", "result");
+        resp.put("loser", loser);
+        sendAllMessage(resp.toJSONString());
+    }
+
+    private void sendMove() {
+        lock.lock();
+        try {
+            JSONObject resp = new JSONObject();
+            resp.put("event", "move");
+            resp.put("a_direction", nextStepA);
+            resp.put("b_direction", nextStepB);
+            sendAllMessage(resp.toJSONString());
+            nextStepA = nextStepB = null;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private boolean nextStep() {    // 等待两名玩家的下一步操作
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            lock.lock();
+            try {
+                if (nextStepA != null && nextStepB != null) {
+                    playerA.getSteps().add(nextStepA);
+                    playerB.getSteps().add(nextStepB);
+                    return true;
+                }
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        return false;
+    }
+
+    private void sendAllMessage(String message) {
+        WebSocketServer.users.get(playerA.getId()).sendMessage(message);
+        WebSocketServer.users.get(playerB.getId()).sendMessage(message);
+    }
+
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            if (nextStep()) {
+                judge();
+                if (status.equals("playing")) {
+                    sendMove();
+                } else {
+                    sendResult();
+                }
+            } else {
+                status = "finished";
+                lock.lock();
+                try {
+                    if (nextStepA == null && nextStepB == null) {
+                        loser = "all";
+                    } else if (nextStepA == null) {
+                        loser = "A";
+                    } else {
+                        loser = "B";
+                    }
+                } finally {
+                    lock.unlock();
+                }
+                sendResult();
+                break;
+            }
         }
     }
 }
